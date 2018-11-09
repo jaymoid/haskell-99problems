1fb6d62d2414f78920060e19d7f5e8964a3de765
James Pittendreigh <james.pittedreigh@skybettingandgaming.com>
2018-11-09 12:15:49 +0000

    Shortening test names, they were a bit unnecessarily long in the output.

--- test/Q01to10ListsSpec.hs
+++ test/Q01to10ListsSpec.hs
@@ -13,45 +13,40 @@ main = hspec spec
 spec :: Spec
 spec = do
 
-  describe "Problem 1: Find the last element of a list." $ do
+  describe "Problem 1" $ do
     it "returns the last element in the provided list" $ do
       myLast [1,2,3,4]  `shouldBe` 4
       myLast ['x','y','z'] `shouldBe` 'z'
 
-  describe "Problem 2: Find the last but one element of a list." $ do
+  describe "Problem 2" $ do
     it "returns the last but one element in the list" $ do
       myButLast [1,2,3,4] `shouldBe` 3
       myButLast ['a'..'z'] `shouldBe` 'y'
 
-  describe "Problem 3:the K'th element of a list. \
-           \The first element in the list is number 1." $ do
+  describe "Problem 3" $ do
     it "returns the kth element in the provided list" $ do
       elementAt [1,2,3] 2 `shouldBe` 2
       elementAt "haskell" 5 `shouldBe` 'e'
 
-  describe "Problem 4: Find the number of elements of a list." $ do
+  describe "Problem 4" $ do
     it "returns the length of a list" $ do
       myLength [123, 456, 789] `shouldBe` 3
       myLength "Hello, world!" `shouldBe` 13
     it "empty list returns 0" $ do
       myLength [] `shouldBe` 0
 
-  describe "Problem 5: Reverse a list." $ do
+  describe "Problem 5" $ do
     it "reverses the list" $ do
       myReverse "A man, a plan, a canal, panama!" `shouldBe` "!amanap ,lanac a ,nalp a ,nam A"
       myReverse [1,2,3,4] `shouldBe` [4,3,2,1]
 
-  describe "Problem 6: Find out whether a list is a palindrome. A palindrome\
-            \can be read forward or backward; e.g. (x a m a x)" $ do
+  describe "Problem 6" $ do
     it "returns true for a palindrome" $ do
       isPalindrome [1,2,3] `shouldBe` False
       isPalindrome "madamimadam" `shouldBe` True
       isPalindrome [1,2,4,8,16,8,4,2,1] `shouldBe` True
 
-  describe "Problem 7: Flatten a nested list structure.\
-           \Transform a list, possibly holding lists as elements into a \
-           \`flat' list by replacing each list with its elements \
-           \(recursively)." $ do
+  describe "Problem 7" $ do
     it "flattens the list" $ do
       flatten (Elem 5)  `shouldBe` [5]
       flatten (List [Elem 1, List [Elem 2, List [Elem 3, Elem 4], Elem 5]])
@@ -59,28 +54,19 @@ spec = do
     it "empty nested lists returns a empty list" $ do
       flatten (List [] :: NestedList Int ) `shouldBe` ([])
 
-  describe "Problem 8 :Eliminate consecutive duplicates of list elements.\n\
-           \If a list contains repeated elements they should be replaced \
-           \with a single copy of the element. The order of the elements \
-           \should not be changed." $ do
+  describe "Problem 8" $ do
     it "should remove consecutive elements" $ do
       compress "aaaabccaadeeee" `shouldBe` "abcade"
     it "should not change lists without consecutive elements" $ do
       compress "abcade" `shouldBe` "abcade"
 
-  describe "Problem 9: Pack consecutive duplicates of list elements into \
-           \sublists. If a list contains repeated elements they should be \
-           \placed in separate sublists." $ do
+  describe "Problem 9" $ do
     it "packs consecutives into sublists" $ do
       pack "112233" `shouldBe` ["11","22","33"]
       pack ['a', 'a', 'a', 'a', 'b', 'c', 'c', 'a', 'a', 'd', 'e', 'e', 'e', 'e'] `shouldBe`  ["aaaa","b","cc","aa","d","eeee"]
     it "lists with no consecutives are in singleton lists" $ do
       pack "123" `shouldBe` ["1","2","3"]
 
-  describe "Problem 10: Run-length encoding of a list. Use the result \
-           \of problem P09 to implement the so-called run-length \
-           \encoding data compression method. Consecutive duplicates of \
-           \elements are encoded as lists (N E) where N is the number \
-           \of duplicates of the element E." $ do
+  describe "Problem 10" $ do
      it "run length encodes the list" $ do
        encode "aaaabccaadeeee" `shouldBe` [(4,'a'),(1,'b'),(2,'c'),(2,'a'),(1,'d'),(4,'e')]
--- test/Q11to20ListsContinuedSpec.hs
+++ test/Q11to20ListsContinuedSpec.hs
@@ -13,51 +13,48 @@ main = hspec spec
 spec :: Spec
 spec = do
 
-  describe "Problem 11: Modified run-length encoding." $ do
+  describe "Problem 11" $ do
     it "returns a list with singles and multiples" $ do
       encodeModified "aaaabccaadeeee" `shouldBe` [Multiple 4 'a',Single 'b',Multiple 2 'c', Multiple 2 'a',Single 'd',Multiple 4 'e']
 
-  describe "Problem 12: Decode a run-length encoded list." $ do
+  describe "Problem 12" $ do
     it "returns a decoded list from an encoded list" $ do
       decodeModified [Multiple 4 'a',Single 'b',Multiple 2 'c',
         Multiple 2 'a',Single 'd',Multiple 4 'e'] `shouldBe` "aaaabccaadeeee"
 
-  describe "Problem 13: Run-length encoding of a list (direct solution)." $ do
+  describe "Problem 13" $ do
     it "returns a list with singles and multiples" $ do
       encodeDirect "aaaabccaadeeee" `shouldBe` [Multiple 4 'a',Single 'b',Multiple 2 'c', Multiple 2 'a',Single 'd',Multiple 4 'e']
 
-  describe "Problem 14: Duplicate the elements of a list." $ do
+  describe "Problem 14" $ do
     it "repeats each element in the list, once" $ do
       dupli [1, 2, 3] `shouldBe` [1,1,2,2,3,3]
       dupli "hello world" `shouldBe` "hheelllloo  wwoorrlldd"
 
-  describe "Problem 15: Replicate the elements of a list a given number \
-            \times." $ do
+  describe "Problem 15" $ do
       it "repeats each element in the list n times" $ do
         repli [1, 2, 3] 2 `shouldBe` [1,1,2,2,3,3]
         repli "abc" 3`shouldBe` "aaabbbccc"
 
-  describe "Problem 16: Drop every N'th element from a list." $ do
+  describe "Problem 16" $ do
     it "drops every N'th element in the list" $ do
       dropEvery "abcdefghik" 3 `shouldBe` "abdeghk"
 
-  describe "Problem 17: Split a list into two parts; the length of the first \
-           \part is given." $ do
+  describe "Problem 17" $ do
     it "splits a list by the given index" $ do
       split "abcdefghik" 3 `shouldBe` ("abc", "defghik")
 
-  describe "Problem 18: Extract a slice from a list." $ do
-    it "Extracts a section of the list from the two indices" $ do
+  describe "Problem 18" $ do
+    it "wxtracts a section of the list from the two indices" $ do
       slice ['a','b','c','d','e','f','g','h','i','k'] 3 7 `shouldBe` "cdefg"
 
-  describe "Problem 19: Rotate a list N places to the left. \
-           \Hint: Use the predefined functions length and (++)." $ do
-    it "Rotates a list elements by a specified amount" $ do
+  describe "Problem 19" $ do
+    it "rotates a list elements by a specified amount" $ do
       rotate ['a','b','c','d','e','f','g','h'] 3 `shouldBe` "defghabc"
     it "Rotates a list elements by a specified negative amount" $ do
       rotate ['a','b','c','d','e','f','g','h'] (-2) `shouldBe` "ghabcdef"
 
-  describe "Problem 20: Remove the K'th element from a list." $ do
-    it "Removes and returns Kth element from a list" $ do
+  describe "Problem 20" $ do
+    it "removes and returns Kth element from a list" $ do
       removeAt 2 "abcd" `shouldBe` ('b',"acd")
       removeAt 4 "abcd" `shouldBe` ('d',"abc")
