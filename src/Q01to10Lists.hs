4e1994fe858c63f2f198c062cd30db0843347072
James Pittendreigh <james.pittedreigh@skybettingandgaming.com>
2019-04-29 17:14:12 +0100

    Adding some (old) solutions

--- src/Q01to10Lists.hs
+++ src/Q01to10Lists.hs
@@ -1,46 +1,127 @@
+{-# LANGUAGE ParallelListComp #-}
 module Q01to10Lists where
 
 -- https://wiki.haskell.org/99_questions/1_to_10
 
 --1
 myLast :: [a] -> a
-myLast = undefined
+myLast (x:xs) = case xs of
+  [] -> x
+  otherwise -> myLast1 xs
+
+myLast1 :: [a] -> a
+myLast1 = foldr1 (const id)
+
+myLast2 :: [a] -> a
+myLast2 = foldl (flip const) undefined
+
 
 --2
 myButLast :: [a] -> a
-myButLast = undefined
+myButLast (x1:x2:xs) = case xs of
+  [] -> x1
+  otherwise -> myButLast xs
+
+myButLast1 :: [a] -> a
+myButLast1 xs = case parse xs of
+    Nothing -> error "Not enough values in list"
+    Just x -> x
+  where parse = fst . foldl (\b a -> (snd b, Just a)) (Nothing, Nothing)
+
+myButLast2 :: [a] -> a
+myButLast2 = last . init
+
 
 --3
 elementAt :: (Integral b) => [a] -> b -> a
-elementAt = undefined
+elementAt xs index = go 1 xs
+  where go count (x:xs)
+         | count < index = go (count + 1) xs
+         | otherwise = x
+
+elementAt1 :: (Integral b) => [a] -> b -> a
+elementAt1 xs n = snd . last $ [(i, x) | x <-xs | i <- [1..n]]
+
+-- or same as above with zip
+elementAt2 :: (Integral b) => [a] -> b -> a
+elementAt2 xs n = snd . last $ zip [1..n] xs
+
 
 --4
 myLength :: (Integral b) => [a] -> b
-myLength = undefined
+myLength xs = go 0 xs
+  where go n [] = n
+        go n (x:xs) = go (n+1) xs
+
+myLength1 :: (Integral b) => [a] -> b
+myLength1 = foldr (\_ count ->  count + 1) 0
+
 
 --5
 myReverse :: [a] -> [a]
-myReverse = undefined
+myReverse = foldr (\a b -> b ++ [a]) []
+
+myReverse1 :: [a] -> [a]
+myReverse1 [] = []
+myReverse1 (x:xs) =  myReverse xs ++ x : []
+
 
 --6
 isPalindrome :: (Eq a) => [a] -> Bool
-isPalindrome = undefined
+isPalindrome a = a == (myReverse a)
+
 
 -- 7
 --In Haskell: We have to define a new data type, because lists in Haskell are homogeneous.
-data NestedList a = Elem a | List [NestedList a] deriving (Show, Eq)
+data NestedList a =
+  Elem a |
+  List [NestedList a]
+  deriving (Show, Eq)
 
 flatten :: NestedList a -> [a]
-flatten = undefined
+flatten (Elem x) = [x]
+flatten (List []) = []
+flatten (List (x:xs)) = flatten x ++ (flatten $ List xs)
+
+flatten1 :: NestedList a -> [a]
+flatten1 (Elem x) = [x]
+flatten1 (List x) = concatMap flatten x
+
+flatten2 :: NestedList a -> [a]
+flatten2 (Elem x) = [x]
+flatten2 (List x) = x >>= flatten
+
 
 --8
 compress :: Eq a => [a] -> [a]
-compress = undefined
+compress [] = []
+compress [x] = [x]
+compress (x:xs) =
+  let grouped = if (x /= head xs) then x:[] else []
+  in grouped ++ compress xs
+
+compress1 :: Eq a => [a] -> [a]
+compress1 = foldr (\e xs -> if (xs == [] || e /= head xs) then e:xs else xs) []
+
 
 --9
 pack :: Eq a => [a] -> [[a]]
-pack = undefined
+pack xs = go [] xs
+  where
+    go [] [] = []
+    go [] (x:xs) = go (x:[]) xs
+    go ys [] = [ys]
+    go ys (x:xs) = if (x `elem` ys) then go (x:ys) xs else [ys] ++ (go [x] xs)
+
+pack1 :: Eq a => [a] -> [[a]]
+pack1 [] = []
+pack1 (x:xs) = (x : takeWhile (==x) xs) : pack (dropWhile (==x) xs)
+
 
 --10
 encode :: Eq a => [a] -> [(Int, a)]
-encode = undefined
+encode = map (\xs -> (length xs, head xs)) . pack
+
+encode1 :: Eq a => [a] -> [(Int, a)]
+encode1 = foldr convert [] . pack
+  where convert a b = (length a, head a) : b
--- src/Q11to20ListsContinued.hs
+++ src/Q11to20ListsContinued.hs
@@ -2,46 +2,96 @@ module Q11to20ListsContinued where
 
 -- https://wiki.haskell.org/99_questions/11_to_20
 
+import Q01to10Lists
+
 data ListItem a = Single a
                 | Multiple Int a
                   deriving (Show, Eq)
 
 -- 11
 encodeModified :: (Eq a) => [a] -> [ListItem a]
-encodeModified = undefined
+encodeModified = map (uncurry toListItem) . encode
+  where toListItem n e = case n of
+          1         -> Single e
+          otherwise -> Multiple n e
+
 
 -- 12
 decodeModified :: [ListItem a] -> [a]
-decodeModified = undefined
+decodeModified = concatMap fromListItem
+  where fromListItem (Single i)     = [i]
+        fromListItem (Multiple n i) = take n $ repeat i
+
 
 -- 13
 encodeDirect :: Eq a => [a] -> [ListItem a]
-encodeDirect = undefined
+encodeDirect []       = []
+encodeDirect xs@(x:_) = firstGroup ++ (encodeDirect restOfList)
+  where
+    firstGroup = (: []) $ toListItem $ takeWhile (== x) xs
+    restOfList = dropWhile (== x) xs
+
+toListItem :: [a] -> ListItem a
+toListItem xs@(x:_) = case length xs of
+        1         -> Single x
+        otherwise -> Multiple (length xs) x
+
 
 -- 14
 dupli :: [a] -> [a]
-dupli = undefined
+dupli [] = []
+dupli (x:xs) = x : x : (dupli xs)
+
 
 -- 15
 repli :: (Integral b) => [a] -> b -> [a]
-repli = undefined
+repli [] _ = []
+repli (x:xs) n = replicateElement n x ++ repli xs n
+  where
+    replicateElement 0 _ = []
+    replicateElement n x = x : replicateElement (n-1) x
+
 
 -- 16
 dropEvery :: (Integral b) => [a] -> b -> [a]
-dropEvery = undefined
+dropEvery xs skipEvery = go xs 1
+  where
+    go []     _ = []
+    go (x:xs) n
+      | n `mod` skipEvery == 0 = go xs (n+1)
+      | otherwise              = x : go xs (n+1)
+
 
 -- 17
 split :: (Integral b) => [a] -> b -> ([a], [a])
-split = undefined
+split xs n = (firstBit, secondBit)
+ where firstBit  = take nInt xs
+       secondBit = drop nInt xs
+       nInt      = fromIntegral n
+
 
 -- 18
 slice :: (Integral b) => [a] -> b -> b -> [a]
-slice = undefined
+slice xs start end = go xs 1
+  where
+    go [] _ = []
+    go (x:xs) n
+      | n >= start && n <= end = x : go xs (n + 1)
+      | otherwise = go xs (n + 1)
+
 
 -- 19
 rotate :: (Integral b) => [a] -> b -> [a]
-rotate = undefined
+rotate xs n = drop offset xs ++ take offset xs
+  where
+    offset = calcOffset $ fromIntegral n
+    calcOffset x = if (x<0) then (length xs) - (abs x) else x
+
 
 -- 20
 removeAt :: (Integral a) => a -> [b] -> (b, [b])
-removeAt = undefined
+removeAt n xs = (xs!!(fromIntegral n - 1), listCreate 1 xs)
+  where listCreate _     []     = []
+        listCreate count (x:xs) = if (count == n)
+                                    then   listCreate (count+1) xs
+                                    else x:listCreate (count+1) xs
